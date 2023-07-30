d785f07d2ccf2d97a3d6d3527b77c9691a4eefb1
marin.ladovic <marin.ladovic@bozic.io>
2023-07-30 12:22:04 +0200

    fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-const { fontFamily } = require("tailwindcss/defaultTheme")
+import animatePlugin from "tailwindcss-animate"
+import radixPlugin from "tailwindcss-radix"
+import defaultTheme from "tailwindcss/defaultTheme.js"
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -14,10 +16,19 @@ export default {
     extend: {
       colors: {
         border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
+        input: {
+          DEFAULT: "hsl(var(--input))",
+          invalid: "hsl(var(--input-invalid))",
+        },
+        ring: {
+          DEFAULT: "hsl(var(--ring))",
+          invalid: "hsl(var(--foreground-danger))",
+        },
         background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        foreground: {
+          DEFAULT: "hsl(var(--foreground))",
+          danger: "hsl(var(--foreground-danger))",
+        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
@@ -27,8 +38,8 @@ export default {
           foreground: "hsl(var(--secondary-foreground))",
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
         muted: {
           DEFAULT: "hsl(var(--muted))",
@@ -48,21 +59,58 @@ export default {
         },
       },
       borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        // 1rem = 16px
+        /** 80px size / 84px high / bold */
+        mega: ["5rem", { lineHeight: "5.25rem", fontWeight: "700" }],
+        /** 56px size / 62px high / bold */
+        h1: ["3.5rem", { lineHeight: "3.875rem", fontWeight: "700" }],
+        /** 40px size / 48px high / bold */
+        h2: ["2.5rem", { lineHeight: "3rem", fontWeight: "700" }],
+        /** 32px size / 36px high / bold */
+        h3: ["2rem", { lineHeight: "2.25rem", fontWeight: "700" }],
+        /** 28px size / 36px high / bold */
+        h4: ["1.75rem", { lineHeight: "2.25rem", fontWeight: "700" }],
+        /** 24px size / 32px high / bold */
+        h5: ["1.5rem", { lineHeight: "2rem", fontWeight: "700" }],
+        /** 16px size / 20px high / bold */
+        h6: ["1rem", { lineHeight: "1.25rem", fontWeight: "700" }],
+
+        /** 32px size / 36px high / normal */
+        "body-2xl": ["2rem", { lineHeight: "2.25rem" }],
+        /** 28px size / 36px high / normal */
+        "body-xl": ["1.75rem", { lineHeight: "2.25rem" }],
+        /** 24px size / 32px high / normal */
+        "body-lg": ["1.5rem", { lineHeight: "2rem" }],
+        /** 20px size / 28px high / normal */
+        "body-md": ["1.25rem", { lineHeight: "1.75rem" }],
+        /** 16px size / 20px high / normal */
+        "body-sm": ["1rem", { lineHeight: "1.25rem" }],
+        /** 14px size / 18px high / normal */
+        "body-xs": ["0.875rem", { lineHeight: "1.125rem" }],
+        /** 12px size / 16px high / normal */
+        "body-2xs": ["0.75rem", { lineHeight: "1rem" }],
+
+        /** 18px size / 24px high / semibold */
+        caption: ["1.125rem", { lineHeight: "1.5rem", fontWeight: "600" }],
+        /** 12px size / 16px high / bold */
+        button: ["0.75rem", { lineHeight: "1rem", fontWeight: "700" }],
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
@@ -71,5 +119,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animatePlugin, radixPlugin],
 }
